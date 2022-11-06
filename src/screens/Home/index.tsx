96a3d1fb6bd1bc9783b32bbb88c2885a09e9a818
Nino <ninodm.dev@gmail.com>
2022-11-06 13:16:41 -0300

    feat: initial commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,13 @@
+import S from './styles';
+
+const Home: React.FC = () => {
+    return (
+        <S.Container>
+            <S.Title>
+                text
+            </S.Title>
+        </S.Container>
+    );
+}
+
+export default Home;
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native';
+
+const S = {
+    Container: styled.View`
+        flex: 1;
+        justify-content: center;
+        align-items: center;
+    `,
+
+    Title: styled.Text`
+        color: red;
+    `    
+};
+
+export default S;
